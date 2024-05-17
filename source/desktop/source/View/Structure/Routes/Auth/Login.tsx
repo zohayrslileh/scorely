f42dbd8ccdc0cb385e12853f9530cfd5e2a6c341
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-18 00:29:03 +0100

    save

--- source/desktop/source/View/Structure/Routes/Auth/Login.tsx
+++ source/desktop/source/View/Structure/Routes/Auth/Login.tsx
@@ -1,4 +1,9 @@
+import TextInput from "@/Tools/MaterialUI/TextInput"
+import Authentication from "@/Core/Authentication"
+import { useNavigate } from "react-router-dom"
+import useForm, { Form } from "@/Tools/Form"
 import styled from "@emotion/styled"
+import { useCallback } from "react"
 
 /**
  * Login
@@ -7,9 +12,25 @@ import styled from "@emotion/styled"
  */
 export default function () {
 
+    const naviagte = useNavigate()
+
+    const { value, update } = useForm(() => new LoginForm)
+
+    const login = useCallback(async function () {
+
+        await Authentication.login(value)
+
+        naviagte("")
+
+    }, [value])
+
     return <Container>
 
-        <h1>Login</h1>
+        <Form onSubmit={login}>
+            <TextInput placeholder="Username" type="text" value={value.username || ""} onChange={value => update.username(value || undefined)} />
+            <TextInput placeholder="Password" type="password" value={value.password || ""} onChange={value => update.password(value || undefined)} />
+            <button>Login</button>
+        </Form>
 
     </Container>
 }
@@ -20,3 +41,8 @@ export default function () {
  */
 const Container = styled.div`
 `
+
+class LoginForm {
+    username: string | undefined
+    password: string | undefined
+}
