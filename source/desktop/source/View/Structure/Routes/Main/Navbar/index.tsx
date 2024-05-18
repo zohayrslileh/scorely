7743041df644994699ad1f83f346600feffe05d3
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-18 22:56:57 +0100

    save

---
+++ source/desktop/source/View/Structure/Routes/Main/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled"
+
+/**
+ * Navbar
+ * 
+ * @returns 
+ */
+export default function () {
+
+    return <Container>
+        Navbar
+    </Container>
+}
+
+/**
+ * Container
+ * 
+ */
+const Container = styled.div`
+`
---
+++ source/desktop/source/View/Structure/Routes/Main/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled"
+
+/**
+ * Sidebar
+ * 
+ * @returns 
+ */
+export default function () {
+
+    return <Container>
+        Sidebar
+    </Container>
+}
+
+/**
+ * Container
+ * 
+ */
+const Container = styled.div`
+`
