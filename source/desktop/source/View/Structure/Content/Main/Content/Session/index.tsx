6efc856c5ab4cee79ee03d678510d5b80b7636cf
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-19 19:59:01 +0100

    Home

--- source/desktop/source/View/Structure/Content/Main/Content/Session/index.tsx
+++ source/desktop/source/View/Structure/Content/Main/Content/Session/index.tsx
@@ -1,6 +1,9 @@
 import PendingException from "@/View/Exception/Exceptions/Pending"
 import manager from "@/Models/Server/Socket"
 import { Throw } from "@/Tools/Exception"
+import { useCallback } from "react"
+import EventError from "@/Tools/Socket/EventError"
+import Button from "@/View/Components/Button"
 
 /**
  * Session
@@ -21,11 +24,33 @@ export default function () {
      */
     const error = main.useState<string>("error")
 
+    /**
+     * Join
+     * 
+     */
+    const join = useCallback(async function () {
+
+        try {
+
+            await main.ask("judge-join")
+
+        } catch (error) {
+
+            if (error instanceof EventError) alert(error.message)
+
+        }
+
+    }, [])
+
     // Error status
     if (error) return <Throw exception={new Error(error)} />
 
     // Conniting status
     if (!main.connected) return <Throw exception={new PendingException("connecting")} />
 
-    return <h1>Home</h1>
+    return <div>
+        <h1>Home</h1>
+        <br />
+        <Button onClick={join}>Join</Button>
+    </div>
 }
