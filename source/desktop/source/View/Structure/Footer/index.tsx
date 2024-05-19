cb714a65fa805a328a6379d6274a94a67b30dd0f
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-19 14:07:21 +0100

    save

--- source/desktop/source/View/Structure/Footer/index.tsx
+++ source/desktop/source/View/Structure/Footer/index.tsx
@@ -1,7 +1,9 @@
+import languages from "@/View/Language/Languages"
 import themes from "@/View/Appearance/Themes"
 import Appearance from "@/View/Appearance"
 import Card from "@/View/Components/Card"
 import { Link } from "react-router-dom"
+import Language from "@/View/Language"
 import styled from "@emotion/styled"
 
 /**
@@ -13,6 +15,7 @@ export default function () {
 
     return <Container className="animation">
         {themes.map(theme => <button key={theme.key} onClick={() => Appearance.theme = theme} disabled={Appearance.theme === theme}>{theme.name}</button>)}
+        {languages.map(language => <button key={language.key} onClick={() => Language.value = language} disabled={Language.value === language}>{language.name}</button>)}
         <Link to="/connect">Connect</Link>
     </Container>
 }
