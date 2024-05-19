9bfb4955ccb32ff6d01cec1449f0914d0c5452e4
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-19 10:45:32 +0100

    Connect

--- source/desktop/source/View/Structure/Content/index.tsx
+++ source/desktop/source/View/Structure/Content/index.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import PendingException from "@/View/Exception/Exceptions/Pending"
 import { lazy, useState, Suspense } from "react"
 import { Throw } from "@/Tools/Exception"
+import Exception from "@/View/Exception"
 import Server from "@/Models/Server"
+import styled from "@emotion/styled"
 
 const Connect = lazy(() => import("./Connect"))
 const Auth = lazy(() => import("./Auth"))
@@ -27,15 +29,31 @@ export default function () {
      */
     return <BrowserRouter>
 
-        <Suspense fallback={<Throw exception={new PendingException} />}>
+        <Container>
 
-            <Routes>
-                <Route index element={server ? <Main /> : <Navigate to="/connect" />} />
-                <Route path="/auth" element={server ? <Auth /> : <Navigate to="/connect" />} />
-                <Route path="/connect" element={<Connect value={server} onChange={setServer} />} />
-            </Routes>
+            <Exception>
 
-        </Suspense>
+                <Suspense fallback={<Throw exception={new PendingException} />}>
+
+                    <Routes>
+                        <Route index element={server ? <Main /> : <Navigate to="/connect" />} />
+                        <Route path="/auth" element={server ? <Auth /> : <Navigate to="/connect" />} />
+                        <Route path="/connect" element={<Connect value={server} onChange={setServer} />} />
+                    </Routes>
+
+                </Suspense>
+
+            </Exception>
+
+        </Container>
 
     </BrowserRouter>
 }
+
+/**
+ * Container
+ * 
+ */
+const Container = styled.div`
+    position: relative;
+`
