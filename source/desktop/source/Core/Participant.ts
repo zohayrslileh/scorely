785f43a4ab7b21e3416a467173e5b878e72a2205
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-20 15:36:47 +0100

    save

---
+++ source/desktop/source/Core/Participant.ts
@@ -0,0 +1,68 @@
+import request from "@/Models/Server/Request"
+import zod from "zod"
+
+/*
+|-----------------------------
+|  Participant
+|-----------------------------
+|
+| 
+*/
+export default class Participant {
+
+    /**
+     * Id
+     * 
+     */
+    private readonly id: number
+
+    /**
+     * Constructor method
+     * 
+     */
+    public constructor(id: unknown) {
+
+        // Schema
+        const schema = zod.number()
+
+        // Validate and set id
+        this.id = schema.parse(id)
+    }
+
+    /**
+     * Read method
+     * 
+     * @returns
+     */
+    public async read() {
+
+        return await request<PrimitiveParticipant>({ url: `/participant/${this.id}` })
+    }
+
+    /**
+     * Create method
+     * 
+     * @returns
+     */
+    public static async create(data: unknown) {
+
+        // Schema
+        const schema = zod.object({
+            name: zod.string().max(50)
+        })
+
+        // Create participant
+        const participant = await request<PrimitiveParticipant>({ method: "POST", url: "/participant", data: schema.parse(data) })
+
+        return new this(participant.id)
+    }
+
+}
+
+/**
+ * Primitive Participant
+ * 
+ */
+interface PrimitiveParticipant {
+    id: number
+}
