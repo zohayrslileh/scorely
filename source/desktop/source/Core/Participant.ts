9bcccdac0ad3f4f61a7a1cd05120d977491892e1
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-21 17:11:22 +0100

    Participant

--- source/desktop/source/Core/Participant.ts
+++ source/desktop/source/Core/Participant.ts
@@ -17,26 +17,22 @@ export default class Participant {
     public readonly id: number
 
     /**
+     * Name
+     * 
+     */
+    public readonly name: string
+
+    /**
      * Constructor method
      * 
      */
-    public constructor(id: unknown) {
+    private constructor(primitiveParticipant: PrimitiveParticipant) {
 
-        // Schema
-        const schema = zod.number()
+        // Set id
+        this.id = primitiveParticipant.id
 
-        // Validate and set id
-        this.id = schema.parse(id)
-    }
-
-    /**
-     * Read method
-     * 
-     * @returns
-     */
-    public async read() {
-
-        return await request<PrimitiveParticipant>({ url: `/participant/${this.id}` })
+        // Set name
+        this.name = primitiveParticipant.name
     }
 
     /**
@@ -54,25 +50,55 @@ export default class Participant {
         // Create participant
         const participant = await request<PrimitiveParticipant>({ method: "POST", url: "/participant", data: schema.parse(data) })
 
-        return new this(participant.id)
+        return new this(participant)
     }
 
     /**
-     * Record
+     * Record method
      * 
      * @returns
      */
     public static async record() {
 
-        return await request<PrimitiveParticipant[]>({ url: "/participant" })
+        // Get participant
+        const participants = await request<PrimitiveParticipant[]>({ url: "/participant" })
+
+        return participants.map(participant => new this(participant))
+    }
+
+    /**
+     * Find method
+     * 
+     * @returns
+     */
+    public static async find(id: unknown) {
+
+        // Schema
+        const schema = zod.number()
+
+        // Get entity
+        const entity = await request<PrimitiveParticipant>({ url: `/participant/${schema.parse(id)}` })
+
+        return new this(entity)
+    }
+
+    /**
+     * Delete method
+     * 
+     * @returns
+     */
+    public async delete() {
+
+        await request({ method: "DELETE", url: `/participant/${this.id}` })
     }
 
 }
 
 /**
- * Primitive Participant
+ * Primitive
  * 
  */
-interface PrimitiveParticipant {
+export interface PrimitiveParticipant {
     id: number
+    name: string
 }
