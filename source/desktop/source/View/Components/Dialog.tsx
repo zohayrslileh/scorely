9324763701d4cdb0994d7810aab8fa938dda2c3c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-28 16:57:42 +0100

    save

--- source/desktop/source/View/Components/Dialog.tsx
+++ source/desktop/source/View/Components/Dialog.tsx
@@ -1,6 +1,4 @@
 import Appearance from "@/View/Appearance"
-import { Dialog } from "@headlessui/react"
-import { keyframes } from "@emotion/react"
 import styled from "@emotion/styled"
 
 /**
@@ -8,13 +6,11 @@ import styled from "@emotion/styled"
  * 
  * @returns 
  */
-export default function ({ children, ...props }: React.ComponentProps<typeof Dialog>) {
+export default function ({ children, ...props }: React.ComponentProps<typeof Container>) {
 
-    return <Dialog {...props}>
-        <Container>
-            {typeof children === "function" ? undefined : children}
-        </Container>
-    </Dialog>
+    return <Container {...props}>
+        {typeof children === "function" ? undefined : children}
+    </Container>
 }
 
 /**
@@ -23,32 +19,4 @@ export default function ({ children, ...props }: React.ComponentProps<typeof Dia
  */
 const Container = styled.div`
     background-color: ${() => Appearance.theme.schema.BACKGROUND_PRIMARY.rgba()};
-    color: ${() => Appearance.theme.schema.CONTENT_COLOR.rgba()};
-    position: fixed;
-    z-index: 9999;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    margin: auto;
-    min-width: 100px;
-    min-height: 100px;
-    width: fit-content;
-    height: fit-content;
-    border-radius: 10px;
-    padding: 10px;
-        
-    animation: ${keyframes`
-
-        0% {
-            scale: 1.1;
-            opacity: 0;
-        }
-
-        100% {
-            scale: 1;
-            opacity: 1;
-        }
-        
-    `} ease 300ms;
 `
