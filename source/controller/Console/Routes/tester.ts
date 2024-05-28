d86e280a4128d817ecd9ed4103f750310fea8813
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-28 20:28:28 +0100

    Tester 🧪

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,3 +1,6 @@
+import { writeFile } from "fs/promises"
+import sleep from "@/Tools/Sleep"
+import puppeteer from "puppeteer"
 
 /*
 |-----------------------------
@@ -7,6 +10,30 @@
 |
 */
 export default async function () {
+
+    const browser = await puppeteer.launch({ headless: false })
+
+    const context = browser.defaultBrowserContext()
+
+    await context.overridePermissions("https://www.google.com", ["geolocation"])
+
+    const page = await browser.newPage()
+
+    await page.setUserAgent("com.google.GoogleMobile/111.0 iPhone/13.5.1 hw/iPhone10_3")
+
+    await page.setViewport({ width: 375, height: 812 })
+
+    await page.setGeolocation({ latitude: 36.3619394, longitude: 127.3958884 })
+
+    await page.goto("https://www.google.com/search?q=apple")
+
+    await sleep(1000)
+
+    await page.goto("https://www.google.com/")
+
+    await writeFile("storage/screenshot.png", await page.screenshot())
+
+    // await browser.close()
 
     console.log("The test completed successfully 🧪 ")
 }
