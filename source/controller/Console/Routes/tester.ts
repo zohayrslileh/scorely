bc7f91e0ecc34eb7a3280b084c860e9e1a3f8b3e
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-29 13:39:31 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,5 +1,3 @@
-import puppeteer from "puppeteer"
-import sleep from "@/Tools/Sleep"
 
 /*
 |-----------------------------
@@ -9,52 +7,6 @@ import sleep from "@/Tools/Sleep"
 |
 */
 export default async function () {
-
-    const browser = await puppeteer.launch()
-
-    const context = browser.defaultBrowserContext()
-
-    await context.overridePermissions("https://www.google.com", ["geolocation"])
-
-    const page = await browser.newPage()
-
-    page.setDefaultTimeout(0)
-
-    await page.setUserAgent("com.google.GoogleMobile/111.0 iPhone/13.5.1 hw/iPhone10_3")
-
-    await page.setViewport({ width: 375, height: 812 })
-
-    await page.setGeolocation({ latitude: 28.6282547, longitude: 77.2202781 })
-
-    await page.goto("https://www.google.com/search?q=apple")
-
-    await sleep(1000)
-
-    await page.goto("https://www.google.com/")
-
-    const textarea = await page.$("textarea")
-
-    if (!textarea) throw new Error
-
-    const recorder = await page.screencast({ path: "storage/record.webm" })
-
-    await sleep(2000)
-
-    await textarea.focus()
-
-    await textarea.type("Free vps")
-
-    await textarea.press("Enter")
-
-    await sleep(3000)
-
-    await page.goto("https://whatismyipaddress.com/")
-
-    await sleep(3000)
-
-    await recorder.stop()
-
-    await browser.close()
 
     console.log("The test completed successfully 🧪 ")
 }
